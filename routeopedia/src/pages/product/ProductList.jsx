b4473a7ec927208f6d45b51ec2da11a82b4d5abc
sdmule <mulesaurabh45@gmail.com>
2026-07-27 19:04:23 +0530

    Showing all products on UI

--- routeopedia/src/pages/product/ProductList.jsx
+++ routeopedia/src/pages/product/ProductList.jsx
@@ -1,5 +1,27 @@
+import { Link } from "react-router-dom";
+import { allProducts } from "../../data/products";
+
 function ProductList() {
-  return <>Product List</>;
+  return (
+    <div className="py-2">
+      <h2>All Products</h2>
+      <p>Browse our complete product catalog</p>
+      <div className="mt-3">
+        {allProducts.map((product) => {
+          return (
+            <div key={product.id} className="border rounded p-3 mb-3">
+              <h3>{product.name}</h3>
+              <p>Price: ${product.price}</p>
+              <p>Category: ${product.category}</p>
+              <Link to="" className="btn btn-outline-success">
+                View Details
+              </Link>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default ProductList;
