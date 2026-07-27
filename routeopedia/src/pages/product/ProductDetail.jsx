8cd92b2000b17d5b6ca8fa6d0777604b05cb97cc
sdmule <mulesaurabh45@gmail.com>
2026-07-27 19:59:07 +0530

    Filter products based on Products

--- routeopedia/src/pages/product/ProductDetail.jsx
+++ routeopedia/src/pages/product/ProductDetail.jsx
@@ -1,7 +1,31 @@
+import { Link, useParams } from "react-router-dom";
+import { allProducts } from "../../data/products";
+
 function ProductDetail() {
+  const { id } = useParams();
+  const product = allProducts.find((p) => p.id == parseInt(id));
+
+  if (!product) {
+    return (
+      <div className="m-3 p-3 border">
+        <h1>Product Not Found</h1>
+        <p>Product with ID "{id}" doesn't exist.</p>
+        <Link to="/products" className="btn btn-outline-success my-2">
+          Back to Products
+        </Link>
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <h1>Name</h1>
+    <div className="m-3 p-3 border">
+      <Link to="/products" className="btn btn-outline-success my-2">
+        Back to Products
+      </Link>
+      <h1>{product.name}</h1>
+      <p>Price: ${product.price}</p>
+      <p>Category: ${product.category}</p>
+      <p>Dynamic route with ID: {id}</p>
     </div>
   );
 }
