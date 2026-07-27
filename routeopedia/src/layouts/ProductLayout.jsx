31a6fde92ee1fc6a4eca84b25efc3256ecf1f80b
sdmule <mulesaurabh45@gmail.com>
2026-07-27 21:46:33 +0530

    Nested Routes

---
+++ routeopedia/src/layouts/ProductLayout.jsx
@@ -0,0 +1,20 @@
+function ProductLayout() {
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-12">
+          <h1 className="mb-3">Products</h1>
+          <p>Browse our complete product catalog</p>
+
+          <nav className="mb-3 border rounded">
+            <div className="p-3">
+              <div className="d-flex gap-2 flex-wrap">ALL PRODUCTS</div>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProductLayout;
--- routeopedia/src/routes/AppRouter.jsx
+++ routeopedia/src/routes/AppRouter.jsx
@@ -4,13 +4,23 @@ import Contact from "../pages/Contact";
 import NotFound from "../pages/NotFound";
 import ProductList from "../pages/product/ProductList";
 import ProductDetail from "../pages/product/ProductDetail";
+import ProductLayout from "../layouts/ProductLayout";
+import AllProduct from "../pages/product/productCategory/AllProduct";
+import Electronics from "../pages/product/productCategory/Electronics";
+import Clothing from "../pages/product/productCategory/Clothing";
+import Books from "../pages/product/productCategory/Books";
 
 function AppRouter() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/contact" element={<Contact />} />
-      <Route path="/products" element={<ProductList />} />
+      <Route path="/products" element={<ProductLayout />}>
+        <Route index element={<AllProduct />} />
+        <Route path="electronics" element={<Electronics />} />
+        <Route path="clothing" element={<Clothing />} />
+        <Route path="books" element={<Books />} />
+      </Route>
       <Route path="/products/item/:id" element={<ProductDetail />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
