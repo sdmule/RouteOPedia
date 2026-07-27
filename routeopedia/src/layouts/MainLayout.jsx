39767d026020bcd9fcea34f4e86e55802a0fe992
sdmule <mulesaurabh45@gmail.com>
2026-07-27 18:23:10 +0530

    Assignment of Navigation

--- routeopedia/src/layouts/MainLayout.jsx
+++ routeopedia/src/layouts/MainLayout.jsx
@@ -5,9 +5,9 @@ function MainLayout() {
   return (
     <nav className="navbar navbar-expand-sm bg-body-tertiary">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">
+        <Link className="navbar-brand" to="/">
           <img src={reactLogo} height="30" />
-        </a>
+        </Link>
         <button
           className="navbar-toggler"
           type="button"
@@ -20,47 +20,11 @@ function MainLayout() {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <NavLink className="nav-link" aria-current="page" to="/">
-                Home
-              </NavLink>
-            </li>
+          <ul className="navbar-nav me-auto my-2 mb-lg-0">
             <li className="nav-item">
               <NavLink className="nav-link" to="/contact">
                 Contact
               </NavLink>
-            </li>
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Dropdown
-              </a>
-              <ul className="dropdown-menu">
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Action
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Another action
-                  </a>
-                </li>
-                <li>
-                  <hr className="dropdown-divider" />
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Something else here
-                  </a>
-                </li>
-              </ul>
             </li>
           </ul>
           <form className="d-flex" role="search">
