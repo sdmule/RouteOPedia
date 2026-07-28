35def432bb3bc370f7d4f8cf86aefd2faaa74daa
sdmule <mulesaurabh45@gmail.com>
2026-07-28 12:07:07 +0530

    Fake Login

--- routeopedia/src/pages/auth/Login.jsx
+++ routeopedia/src/pages/auth/Login.jsx
@@ -1,4 +1,14 @@
 function Login() {
+  const handleFakeLogin = (role) => {
+    const users = {
+      admin: { email: "admin@demo.com", name: "Admin User", role: "admin" },
+      customer: {
+        email: "customer@demo.com",
+        name: "Customer User",
+        role: "customer",
+      },
+    };
+  };
   return (
     <div className="container p-4">
       <div className="row justify-content-center">
@@ -13,11 +23,17 @@ function Login() {
             </p>
 
             <div className="d-grid gap-3 mb-4">
-              <button className="btn btn-danger btn-lg">
+              <button
+                className="btn btn-danger btn-lg"
+                onClick={() => handleFakeLogin("admin")}
+              >
                 <i className="bi bi-person-gear me-2"></i>
                 Login as Admin
               </button>
-              <button className="btn btn-primary btn-lg">
+              <button
+                className="btn btn-primary btn-lg"
+                onClick={() => handleFakeLogin("customer")}
+              >
                 <i className="bi bi-person-circle me-2"></i>
                 Login as Customer
               </button>
